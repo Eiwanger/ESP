70c5dfa7f5cc65d3c76b2fa28923e17723f8674c
Stefan <stefeiwanger@t-online.de>
2018-10-23 14:21:13 +0300

    Start assignment linked list

---
+++ ESP/Code_4_linked_list/linked_list.c
@@ -0,0 +1,57 @@
+#include "linked_list.h"
+
+
+/* 
+ Function will create a new list item and add that to end of list
+ create a new element (malloc)
+ add s to data item of new element
+ make link from last element to new element (next)
+ add NULL to new elements next
+ write new index (last+1) to new elements index
+ *ll is pointing to head of the list
+ return -1 as error (no space left), index position in success
+ */
+int add_to_list(linked_list *ll, char*s)
+{
+
+	return 0;
+}
+
+// int dissplays the required element 
+// (pointed by parameter linked_list *ll) in a list
+// Return: -1 if element was not found else 0 ok
+int display_item(linked_list *ll)
+{
+
+	return 0;
+}
+
+
+
+
+// List all elements in a list
+// return: number of elements in list
+int display_list(linked_list *ll)
+{
+
+	return 0;
+}
+
+// search for a data *s from a list
+// Return: NULL if not found else pointer to to data
+
+linked_list * search_from_list(linked_list *ll, char *s)
+{
+
+	return 0;
+}
+
+
+// Delete item from a list and free the space
+// Rest of the list items index will be countet again
+// Return value: -1 on error else length of remaining list
+int delete_from_list(linked_list *ll, int index)
+{
+
+	return 0;
+}
---
+++ ESP/Code_4_linked_list/linked_list.h
@@ -0,0 +1,17 @@
+
+typedef struct linked_list{
+	char* data;
+	int index; /* normally not used with linked lists, just for the exercises */
+	struct linked_list *next;
+} linked_list;
+
+
+int add_to_list(linked_list *ll, char *s);
+
+int display_item(linked_list *ll);
+
+int display_list(linked_list *ll);
+
+linked_list * search_from_list(linked_list *ll, char *s);
+
+int delete_from_list(linked_list *ll, int index);
