eeafc9c23fb5c7372a75757b49118f7894926a53
Stefan <stefeiwanger@t-online.de>
2018-10-28 12:01:13 +0200

    try to find libgtest.a with -I

--- scripts/travisMake.sh
+++ scripts/travisMake.sh
@@ -11,4 +11,4 @@ cd /home/travis/build/Eiwanger/ESP/ESP/Code_4_linked_list
 #g++ -Wall -c -std=c++11 -g linked_list.c linked_list.h -o linked_list_test.o
 #g++ -I ${GTEST_DIR}/include -pthread -std=c++11 linked_list_test_case.o Gtest_main.o libgtest.a linked_list_test.o -g -o unit_test 
 
-g++ -isystem ${GTEST_DIR}/include -pthread -std=c++11 linked_list_test_case.c linked_list.c linked_list.h Gtest_main.c libgtest.a -o unit_test
+g++ -isystem ${GTEST_DIR}/include -I/usr/lib -pthread -std=c++11 linked_list_test_case.c linked_list.c linked_list.h Gtest_main.c libgtest.a -o unit_test
