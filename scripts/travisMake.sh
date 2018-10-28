09aa989ac09e7057b0ac7c681bd77c43b298cb84
Stefan <stefeiwanger@t-online.de>
2018-10-28 11:27:45 +0200

    travis again

--- scripts/travisMake.sh
+++ scripts/travisMake.sh
@@ -1,8 +1,9 @@
 
 cd /home/travis/build/Eiwanger/ESP/ESP/Code_4_linked_list
-g++ -c -std=c++11 -isystem ${GTEST_DIR}/include -I ${GTEST_DIR} Gtest_main.c 
 g++ -isystem -std=c++11 ${GTEST_DIR}/include -I${GTEST_DIR} -pthread -c ${GTEST_DIR}/src/gtest-all.cc
 ar -rv libgtest.a gtest-all.o
+
+g++ -c -std=c++11 -isystem ${GTEST_DIR}/include -I${GTEST_DIR} Gtest_main.c 
 
 g++ -c -std=c++11 -isystem ${GTEST_DIR}/include -I ${GTEST_DIR} linked_list_test_case.c -o linked_list_test_case.o
 g++ -Wall -c -std=c++11 -g linked_list.c -o linked_list_test.o
