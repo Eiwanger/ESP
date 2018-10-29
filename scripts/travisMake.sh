1cc5f10ee5f9f5bfdf42fdeedd644a9345263c03
Stefan <stefeiwanger@t-online.de>
2018-10-29 11:39:08 +0200

    15. error in .travis

--- scripts/travisMake.sh
+++ scripts/travisMake.sh
@@ -5,9 +5,10 @@ ar -rv libgtest.a gtest-all.o
 
 
 
-g++ -c -std=c++11 -isystem ${GTEST_DIR}/include -I${GTEST_DIR} Gtest_main.c -o Gtest_main.o 
+#g++ -c -std=c++11 -isystem ${GTEST_DIR}/include -I${GTEST_DIR} Gtest_main.c -o Gtest_main.o 
 
-g++ -c -std=c++11 -isystem ${GTEST_DIR}/include -I ${GTEST_DIR} linked_list_test_case.c linked_list.h -o linked_list_test_case.o
-g++ -Wall -c -std=c++11 -g linked_list.c linked_list.h -o linked_list_test.o
-g++ -I ${GTEST_DIR}/include -pthread -std=c++11 linked_list_test_case.o Gtest_main.o libgtest.a linked_list_test.o -g -o unit_test 
+#g++ -c -std=c++11 -isystem ${GTEST_DIR}/include -I ${GTEST_DIR} linked_list_test_case.c linked_list.h -o linked_list_test_case.o
+#g++ -Wall -c -std=c++11 -g linked_list.c linked_list.h -o linked_list_test.o
+#g++ -I ${GTEST_DIR}/include -pthread -std=c++11 linked_list_test_case.o Gtest_main.o libgtest.a linked_list_test.o -g -o unit_test 
 
+g++ -isystem ${GTEST_DIR}/include -pthread linked_list_test_case.c linked_list.c linked_list.h Gtest_main.c libgtest.a -o unit_test
